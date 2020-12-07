55c0ff0e69581b7b205ec0dd5eb8bdcdd06a826c
DevNick <nicolastheodoro@gmail.com>
2020-12-07 19:53:50 -0300

    Component ChannelList finalized

---
+++ src/components/ChannelList/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Container, Category, AddCategoryIcon } from './styles';
+
+
+const ChannelList: React.FC = () => { //TS
+    return (
+        <Container>
+            <Category>
+                <span>Canais de texto</span>
+                <AddCategoryIcon />
+            </Category>
+
+            {/* <ChannelButton channelName="chat-livre" />
+            <ChannelButton channelName="trabalho" />
+            <ChannelButton channelName="lolzinho" />
+            <ChannelButton channelName="valorant" />
+            <ChannelButton channelName="csgo" /> */}
+        </Container>
+    )
+};
+
+export default ChannelList;
